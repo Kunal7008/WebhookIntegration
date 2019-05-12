6125189782e0e3452a72e6305e82e64f7f137652
Kunal7008 <45513114+Kunal7008@users.noreply.github.com>
2019-05-12 18:57:51 +0530

    Update Start.java

--- src/test/java/com/testing/webhook/Start.java
+++ src/test/java/com/testing/webhook/Start.java
@@ -5,7 +5,7 @@ public class Start {
 	public void get() {
 	System.setProperty("webdriver.chrome.driver", "C:\\Users\\KUNAL BHATTA\\Desktop\\chromedriver.exe");
 	WebDriver driver= new ChromeDriver();
-	driver.get("https://jio.com");
+	driver.get("https://flipkart.com");
 	String s=driver.getTitle();
 	System.out.println(s);
 }

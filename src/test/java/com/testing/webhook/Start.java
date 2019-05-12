751bfaa140c02dfc57487203f25cd178b470d625
Kunal7008 <45513114+Kunal7008@users.noreply.github.com>
2019-05-12 12:04:42 +0530

    Update Start.java

--- src/test/java/com/testing/webhook/Start.java
+++ src/test/java/com/testing/webhook/Start.java
@@ -1,5 +1,12 @@
 package com.testing.webhook;
 
 public class Start {
-
+@Test
+	public void get() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\KUNAL BHATTA\\Desktop\\chromedriver.exe");
+	WebDriver driver= new ChromeDriver();
+	driver.get("https://jio.com");
+	String s=driver.getTitle();
+	System.out.println(s);
+}
 }
